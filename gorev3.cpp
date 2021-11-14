1c3413656d0aee5c141f9ab9015f73f0497561b7
Hikmet Can Köseoğlu <41568702+Hikmo@users.noreply.github.com>
2021-11-14 18:26:50 +0300

    Add files via upload

---
+++ gorev3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <class T>
+vector<vector<T>> createMatrix(T content, int y, int x){
+    vector<vector<T>> v1;
+    vector<T> v2;
+    for(int i=0; i<y; i++){
+        for(int j=0; j<x; j++){
+            v2.push_back(content);
+        }
+        v1.push_back(v2);
+    }
+    return(v1);
+}
+
+int main(){
+    string content;
+    cin >> content;
+    vector matrix = createMatrix(content,2,3);
+    return 0;
+}
